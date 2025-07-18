bf7fd0008df7ff3ac67744ee87fadf2ee601b978
Abbalolo <verifiedaboki@gmail.com.com>
2025-07-18 23:47:39 +0100

    error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "export", // correct key is 'output'
+   reactStrictMode: true,
   images: {
     unoptimized: true, // required for static export if using next/image
   },
