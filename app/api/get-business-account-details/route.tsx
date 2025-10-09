23d3c96b2c0573dce1d489ed4c2267a0b1b50123
Abbalolo <verifiedaboki@gmail.com.com>
2025-10-10 00:07:58 +0100

    error

--- app/api/get-business-account-details/route.tsx
+++ app/api/get-business-account-details/route.tsx
@@ -3,8 +3,18 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  {
+    db: {
+      schema: "public" 
+    },
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false
+    }
+  }
 );
+
 
 export async function POST(req: Request) {
   try {
